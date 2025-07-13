48377ac712717083d80967ceb0334dc8d0fa440f
urenami <urenami@yahoo.com>
2025-07-13 04:38:59 -0400

    Add front-end form with styling and update picks route

--- routes/picks.js
+++ routes/picks.js
@@ -1,26 +1,37 @@
 // Load modules
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // POST /save-pick: save a betting pick
-router.post('/save-pick', (req, res) => {
+router.post("/save-pick", (req, res) => {
   const newPick = req.body;
 
-  const filePath = path.join(__dirname, '../data/picks.json');
+  const filePath = path.join(__dirname, "../data/picks.json");
 
   let picks = [];
   if (fs.existsSync(filePath)) {
-    picks = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    picks = JSON.parse(fs.readFileSync(filePath, "utf-8"));
   }
 
   picks.push(newPick);
 
   fs.writeFileSync(filePath, JSON.stringify(picks, null, 2));
 
-  res.json({ message: 'Pick saved!' });
+  res.json({ message: "Pick saved!" });
+});
+
+router.get("/get-picks", (req, res) => {
+  const filePath = path.join(__dirname, "../data/picks.json");
+
+  let picks = [];
+  if (fs.existsSync(filePath)) {
+    picks = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  }
+
+  res.json(picks);
 });
 
 // Export router
